f271e36227f71ce5b4fdb3fabf787932bb6719db
hgti32 <127024694+hgti32@users.noreply.github.com>
2026-04-12 21:26:47 +0300

    add class

--- LAB_7/observer.ts
+++ LAB_7/observer.ts
@@ -2,3 +2,29 @@ type EventMap = {
   "order_placed": { orderId: number; total: number };
   "system_alert": { severity: string; message: string };
 };
+
+class EventEmitter { 
+  private listeners: { [K in keyof EventMap]?: Array<(data: EventMap[K]) => void> } = {};
+
+  subscribe<K extends keyof EventMap>(event: K, callback: (data: EventMap[K]) => void) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    (this.listeners[event] as Array<(data: EventMap[K]) => void>).push(callback);
+    return () => {
+      const current = this.listeners[event];
+      if (current) {
+        (this.listeners[event] as any) = current.filter(cb => cb !== callback);
+      }
+      console.log(`[System]: Unsubscribed from "${event}"`);
+    };
+  }
+
+  emit<K extends keyof EventMap>(event: K, data: EventMap[K]) {
+    const eventListeners = this.listeners[event];
+    if (eventListeners) {
+      eventListeners.forEach(callback => callback(data));
+    }
+  }
+}
