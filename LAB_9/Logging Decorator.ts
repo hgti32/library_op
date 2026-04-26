dd3682237221d4639b9b43595017edc23ecbd6cc
hgti32 <127024694+hgti32@users.noreply.github.com>
2026-04-26 13:03:13 +0300

    add function Log

--- LAB_9/Logging Decorator.ts
+++ LAB_9/Logging Decorator.ts
@@ -12,3 +12,35 @@ const defaultLoggerService = {
   log: (msg: string) => console.log(msg),
   error: (msg: string) => console.error(msg),
 };
+
+
+export function Log({ 
+  level = 'INFO', 
+  formatter = defaultJsonFormatter,
+  loggerService = defaultLoggerService 
+}: LoggerOptions = {}) {
+  
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (...args: any[]) {
+      const startTime = performance.now();
+      
+      const createLogEntry = () => ({
+        timestamp: new Date().toISOString(),
+        level,
+        method: propertyKey,
+        args,
+      });
+
+      const emitLog = (logData: any, isError = false) => {
+        const formattedMessage = formatter(logData);
+        if (isError) {
+          loggerService.error(formattedMessage);
+        } else if (level !== 'ERROR') {
+          loggerService.log(formattedMessage);
+        }
+      };
+    };
+  };
+}
