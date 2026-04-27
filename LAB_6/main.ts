29f13b19e0785d4c43d5eb1fe706c0c35e2adb48
hgti32 <127024694+hgti32@users.noreply.github.com>
2026-04-27 13:58:43 +0300

    add class

--- LAB_6/main.ts
+++ LAB_6/main.ts
@@ -1,3 +1,19 @@
 const fs = require('fs');
 const readline = require('readline');
 
+class LargeDataProcessor {
+  static async processLogFile(filePath: string): Promise<void> {
+    console.log(`Starting to process file: ${filePath}...`);
+    const startTime = performance.now();
+
+    const fileStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity,
+    });
+
+    let processedLines = 0;
+    let errorCount = 0;
+  }
+}
